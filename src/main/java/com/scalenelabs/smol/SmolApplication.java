330d0d4c1ff55ac4f2128e20422d33d0d203b92a
Omar Enrique Castro <oecastrom@gmail.com>
2019-01-12 11:06:09 -0600

    fixed imports

---
+++ src/main/java/com/scalenelabs/smol/SmolApplication.java
@@ -0,0 +1,14 @@
+package com.scalenelabs.smol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmolApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/scalenelabs/smol/SmolApplicationTests.java
@@ -0,0 +1,17 @@
+package com.scalenelabs.smol;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
